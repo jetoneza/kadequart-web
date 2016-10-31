93069075b234965c642dd57c2a11e8d75c9ffdb7
Jethro Ordaneza <jet.oneza@gmail.com>
2016-10-31 14:13:29 +0800

    Add initial styling for login page

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Header from 'components/Header'
 import 'styles/core.scss'
 
-export const CoreLayout = ({ children }) => (
+export const CoreLayout = ({ children, location }) => (
   <div className='core-layout'>
-    <Header />
+    { location.pathname !== '/login' && <Header /> }
     <div className="main-content ui main container">
       {children}
     </div>
--- src/routes/Login/components/Login.js
+++ src/routes/Login/components/Login.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 class Login extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Login Page</h1>
+      <div className="login-page">
+        <div className="ui raised wide segment form-wrapper">
+          <form className="ui small form">
+            <div className="field">
+              <div className="ui right icon input">
+                <i className="user icon"></i>
+                <input type="text" name="username" placeholder="Username" />
+              </div>
+            </div>
+            <div className="field">
+              <div className="ui right icon input">
+                <i className="lock icon"></i>
+                <input type="password" name="password" placeholder="Password" />
+              </div>
+            </div>
+            <div className="ui stackable grid">
+              <div className="nine wide column">
+                <div className="ui small blue submit button">Log In</div>
+              </div>
+              <div className="seven wide column signup-link">
+                <Link to="/signup">Sign up here.</Link>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
