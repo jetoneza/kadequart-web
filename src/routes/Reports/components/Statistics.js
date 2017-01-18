207362e3e099f3529087acad8d079754f5e5e010
Jethro Ordaneza <jet.oneza@gmail.com>
2017-01-18 19:02:36 +0800

    Add summary total to reports page

--- src/routes/Reports/components/Statistics.js
+++ src/routes/Reports/components/Statistics.js
@@ -5,6 +5,12 @@ class Statistics extends React.Component {
 
   render() {
     const { fetchingStatistics, data } = this.props.statistics;
+    let summaryTotal = 0
+
+    if (!fetchingStatistics && data) {
+      summaryTotal = data.totalPayin - data.totalPayout
+    }
+
     return (
       <div className="ui stackable grid statistics-section">
         <div className="four wide column center aligned">
@@ -52,6 +58,18 @@ class Statistics extends React.Component {
             </div>
             <div className="label">
               Top Pay Out
+            </div>
+          </div>
+        </div>
+        <div className="sixteen wide column center aligned">
+          <div className={`ui ${summaryTotal > 0 ? 'green' : 'red'} statistic`}>
+            <div className="value">
+              { fetchingStatistics && <div className="ui active inline loader"></div> }
+              { !fetchingStatistics && <i className={`${summaryTotal > 0 ? 'line chart' : 'level down'} icon`}></i> }
+              { !fetchingStatistics && (data && ` ${formatNumber(summaryTotal, '', 0)}`) }
+            </div>
+            <div className="label">
+              {  summaryTotal > 0 ? 'Gain' : 'Loss'}
             </div>
           </div>
         </div>
